7a497907758ebe26ff9d95213c1ad3855e76474e
Lauren Vicknair <lvicknair@greenhouse.io>
2016-05-04 18:35:17 -0400

    added first specific api method

--- app/services/xignite_api_service.rb
+++ app/services/xignite_api_service.rb
@@ -1,27 +1,30 @@
 class XigniteApiService
 	include HTTParty
 
-	def initialize
+	def get_manager(name)
+		get('xHoldings.json/LookupManager', { :Name => name})
 	end
+
+
 
 	private
 
-	def base_url
-		'http://www.xignite.com/'
-	end
-
-	def get(path, params={})
-		url = base_url + path + '?_Token=' + api_token
-		query_string = ''
-		params.each_pair do |key, value|
-			query_string += "&#{key}=#{value}"
+		def base_url
+			'http://www.xignite.com/'
 		end
-		url = url + query_string
-		puts url
-		self.class.get(url)
-	end
 
-	def api_token
-		ENV.fetch('XIGNITE_API_TOKEN')
-	end
+		def get(path, params={})
+			url = base_url + path + '?_Token=' + api_token
+			query_string = ''
+			params.each_pair do |key, value|
+				query_string += "&#{key}=#{value}"
+			end
+			url = url + query_string
+			puts url
+			self.class.get(url)
+		end
+
+		def api_token
+			ENV.fetch('XIGNITE_API_TOKEN')
+		end
 end
