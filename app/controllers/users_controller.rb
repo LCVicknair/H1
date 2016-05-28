164ea0170e7fadf1b7f18ece5b8f273cc9334f33
Lauren Vicknair <lvicknair@greenhouse.io>
2016-05-28 12:21:36 -0400

    fixed welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,18 @@
 class UsersController < ApplicationController
 
-  # def index #home page
-  # 	@user = User.find(params[:id])
-  # end
-
   def show
   	@user = User.find(params[:id])
     @managers = @user.managers
   end
 
-def create
-	@user = User.new(user_params)
-	if @user.save
-		flash[:notice] = "Welcome to Hedgies!"
-		redirect_to new_user_registration_path
-	elsegti fetch
-	end
-
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Welcome to Hedgies!"
+			redirect_to new_user_registration_path
+		else 
+			fetch
+		end
 	end
 
 	private
